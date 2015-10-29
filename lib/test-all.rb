b65abcc80f68d57991130bb258c22e9324c6eace
Luke Adams <luke4450@gmail.com>
2015-10-29 16:09:13 -0500

    Fix syntax error
    Thinking of some other language…

--- lib/test-all.rb
+++ lib/test-all.rb
@@ -2,7 +2,7 @@ MAX_PARALLEL = 3 # Number of Casks to test in parallel. 3 seems reasonable
 CASKS = `brew cask search`.split("\n").shift #Our wonderful lsit of Casks
 
 
-for CASKS[0..10] do |cask|
+for cask in CASKS[0..10] do
 	`brew cask install #{cask}`
 	`brew cask zap #{cask}`
 	`brew cask cleanup`
