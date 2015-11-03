c313ab0cacac526d1cc86b6fda95151a9cdd7be8
Luke Adams <luke4450@gmail.com>
2015-11-02 21:50:34 -0600

    Test

--- lib/test-all.rb
+++ lib/test-all.rb
@@ -1,9 +1,24 @@
 MAX_PARALLEL = 3 # Number of Casks to test in parallel. 3 seems reasonable
-CASKS = `brew cask search`.split("\n").shift #Our wonderful lsit of Casks
+CASKS = `brew cask search`.split("\n").shift #Our wonderful list of Casks
+		#List of all 	#Each line to[] #Remove 1st element (cask info message)
+require 'thread/pool'
 
 
+pool = Thread.pool(MAX_PARALLEL)
+#Magic happens here
 for cask in CASKS[0..10] do
-	`brew cask install #{cask}`
-	`brew cask zap #{cask}`
-	`brew cask cleanup`
+	pool.process do
+		install, install_res = `brew cask install #{cask}`, $?
+		if install_res.exitstatus == 0 do
+			#Install succeeded
+
+			#Remove the cask
+			uninstall, uninstall_res = `brew cask zap #{cask}`, $?
+
+		else
+			#Some error
+			puts "error: Brew failed to install #{cask}"
+		
+		
+	end
 end
